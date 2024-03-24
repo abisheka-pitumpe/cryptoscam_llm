bb6b842bbc45cbc9fa60c7b3d5120f4d5697d270
abisheka-pitumpe <40793632+cabisheka@users.noreply.github.com>
2024-03-24 19:14:11 -0400

    url list

--- ollama.py
+++ ollama.py
@@ -2,7 +2,7 @@ import pandas as pd
 import subprocess
 
 df = pd.read_csv('ocr.csv')
-new_txt = [x.strip() for x in open('new.txt').readlines()]
+new_txt = [x.strip() for x in open('true_positive_urls.txt').readlines()]
 
 prompt = 'You are a financial advisor that responds in JSON. Could this text belong to a website that is a cryptocurrency news site? Say yes or no, nothing else.'
 
