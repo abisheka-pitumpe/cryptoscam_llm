33b04c6c5128a8db1050de58f0c67f5e70782a62
abisheka-pitumpe <40793632+cabisheka@users.noreply.github.com>
2024-03-24 19:09:22 -0400

    Script to run LLM

---
+++ ollama.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import subprocess
+
+df = pd.read_csv('ocr.csv')
+new_txt = [x.strip() for x in open('new.txt').readlines()]
+
+prompt = 'You are a financial advisor that responds in JSON. Could this text belong to a website that is a cryptocurrency news site? Say yes or no, nothing else.'
+
+for index, row in df.iterrows():
+    if row["URL"] not in new_txt:
+        continue
+    input_text = f'{prompt} {row["Text"]}'
+
+    command = ['ollama', 'run', 'llama2', input_text]
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = process.communicate()
+
+    print(f'URL: {row["URL"]}, Response: {stdout.decode()}')
+
+    if stderr:
+        print(stderr.decode())
